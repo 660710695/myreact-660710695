dfd811b7fd199292e558d1771399ecd1fc257286
Gooljila Thaichareon <thaichareon_g@su.ac.th>
2025-08-07 05:58:50 +0000

    lab week4 react

---
+++ gooljila-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './Mylogo.png';
+import './App.css';
+import Hello from './Hello';
+import Bye from './Bye';
+import JSXExamples from './components/JSXExamples';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code> src/App.js</code> and save to reload
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <JSXExamples/>
+      <Bye/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ gooljila-app/src/Bye.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bye() {
+  return (
+    <div>
+      <p>Byebyeeee, I'm killling myself</p>
+    </div>
+  );
+}
+
+export default Bye;
